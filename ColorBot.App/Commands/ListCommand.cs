f20115d9c0b1ab9c3e795ce3e29d8adc1caf087f
Steven Garberg <steven.garberg.jr@gmail.com>
2021-10-12 15:51:57 -0400

    Added list and set commands

---
+++ ColorBot.App/Commands/ListCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace ColorBot.App.Commands
+{
+    public class ListCommand : CommandBase
+    {
+        [Command("list")]
+        public async Task HandleCommandAsync()
+        {
+            var colorRoles = Context.Guild.Roles.Where(r => r.Name.StartsWith("#")).ToArray();
+            
+            if (colorRoles.Length == 0)
+            {
+                await ReplyAsync("There are no color roles currently in use.");
+            }
+            else
+            {
+                var message = $"The following color roles are currently in use:";
+                
+                message = colorRoles.Aggregate(message, (current, role) =>
+                    current + $"{Environment.NewLine}  • {role.Name} - {role.Members.Count()} user(s)");
+                
+                await ReplyAsync(message);
+            } 
+        }
+    }
+}
---
+++ ColorBot.App/Commands/SetCommand.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace ColorBot.App.Commands
+{
+    public class SetCommand : CommandBase
+    {
+        [Command("set")]
+        public async Task HandleCommandAsync(string value)
+        {
+            System.Drawing.Color color;
+            string colorHex;
+            
+            try
+            {
+                color = System.Drawing.ColorTranslator.FromHtml(value);
+                colorHex = $"#{color.R:X2}{color.G:X2}{color.B:X2}";
+            }
+            catch // Inform the user that the color could not be parsed by name or hex value
+            {
+                await ReplyAsync($"{Mention} Color could not be parsed from input. Please try again.");
+                return;
+            }
+            
+            foreach (var id in ((IGuildUser)Context.User).RoleIds)
+            {
+                var userRole = Context.Guild.Roles.FirstOrDefault(r => r.Id == id);
+                if (userRole == null) continue;
+                
+                if (userRole.Name.Contains("#") && userRole.Name != colorHex)
+                {
+                    if (userRole.Members.Count() == 1) // Check if user is the only one in role
+                    {
+                        await userRole.DeleteAsync(); // Delete if so
+                    }
+                    else // Else, simply remove them from the role
+                    {
+                        await ((IGuildUser)Context.User).RemoveRoleAsync(userRole);
+                    }
+                }
+            }
+            
+            var role = Context.Guild.Roles.FirstOrDefault(r => r.Name == colorHex);
+            if (role != null) // Color role exists
+            {
+                if (role.Members.Any(m => m.Id == Context.User.Id))
+                {
+                    await ReplyAsync($"{Mention} You are already the color {colorHex}.");
+                    return;
+                }
+                await ((IGuildUser)Context.User).AddRoleAsync(role);
+            }
+            else // Color role does not exist
+            {
+                var createdRole = await Context.Guild.CreateRoleAsync(colorHex, GuildPermissions.None,
+                    new Color(color.R, color.G, color.B), false, false);
+                
+                await ((IGuildUser)Context.User).AddRoleAsync(createdRole);
+            }
+            
+            await ReplyAsync($"{Mention} Your color has been updated to {colorHex}!");
+        }
+    }
+}
