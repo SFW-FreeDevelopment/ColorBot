824a3115f9f047af4b75497fb394ccbef152b97e
Steven Garberg <steven.garberg.jr@gmail.com>
2021-10-12 14:28:24 -0400

    Added command base and ping command

---
+++ ColorBot.App/Commands/CommandBase.cs
@@ -0,0 +1,9 @@
+﻿using Discord.Commands;
+
+namespace ColorBot.App.Commands
+{
+    public class CommandBase : ModuleBase<SocketCommandContext>
+    {
+        protected string Mention => Context.Message.Author.Mention;
+    }
+}
---
+++ ColorBot.App/Commands/PingCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace ColorBot.App.Commands
+{
+    public class PingCommand : CommandBase
+    {
+        [Command("ping")]
+        public async Task HandleCommandAsync()
+        {
+            await ReplyAsync("I am pinging the server.");
+        }
+    }
+}
