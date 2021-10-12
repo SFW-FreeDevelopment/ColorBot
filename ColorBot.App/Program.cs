17aca302bdd597f5067208d27d2f549e623cee51
Steven Garberg <steven.garberg.jr@gmail.com>
2021-10-12 14:28:15 -0400

    Configured bot

--- ColorBot.App/Program.cs
+++ ColorBot.App/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ColorBot.App
 {
@@ -6,7 +12,61 @@ namespace ColorBot.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RunBotAsync().GetAwaiter().GetResult();
+            Console.ReadKey();
+        }
+
+        private static DiscordSocketClient _client;
+        private static CommandService _commands;
+        private static IServiceProvider _services;
+
+        public static async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            const string token = "REPLACE_ME";
+
+            _client.Log += Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private static Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+        
+        private static async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private static async Task HandleCommandAsync(SocketMessage socketMessage)
+        {
+            var message = socketMessage as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("colorbot ", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
         }
     }
 }
