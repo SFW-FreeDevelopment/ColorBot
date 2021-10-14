5c818eb783d5d7f978b2931b240cd000b4e7d9b2
Steven Garberg <steven.garberg.jr@gmail.com>
2021-10-13 22:22:47 -0400

    Added help command

---
+++ ColorBot.App/Commands/HelpCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace ColorBot.App.Commands
+{
+    public class HelpCommand : CommandBase
+    {
+        [Command("help")]
+        public async Task HandleCommandAsync()
+        {
+            await ReplyAsync($"**The following commands can be used:**{Environment.NewLine}" +
+                             $"  • **ping** - Pings the Discord channel{Environment.NewLine}" +
+                             $"  • **konami** - Displays the Konami code as emojis{Environment.NewLine}" +
+                             $"  • **set \"name or hex value\"** - Updates user color via role{Environment.NewLine}" +
+                             $"  • **list** - Lists all currently used color roles{Environment.NewLine}" +
+                             $"  • **fact** - Removes empty color roles");
+        }
+    }
+}
