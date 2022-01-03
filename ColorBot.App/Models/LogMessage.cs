9fedde7dcc54c9ce0b253abde93187e3559959e7
Steven Garberg <steven.garberg.jr@gmail.com>
2022-01-02 22:25:48 -0500

    Added Log Message entity model

---
+++ ColorBot.App/Models/LogMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ColorBot.App.Models
+{
+    public class LogMessage
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Command { get; set; }
+        public string SubCommand { get; set; }
+        public string Author { get; set; }
+        [NotMapped] public List<string> Mentions { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
