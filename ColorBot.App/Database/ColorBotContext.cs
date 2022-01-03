79a5880dde2996f39695a99dec2f59638a8ef021
Steven Garberg <steven.garberg.jr@gmail.com>
2022-01-02 22:26:04 -0500

    Created ColorBotContext for DbContext

---
+++ ColorBot.App/Database/ColorBotContext.cs
@@ -0,0 +1,13 @@
+﻿using ColorBot.App.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ColorBot.App.Database
+{
+    public class ColorBotContext : DbContext
+    {
+        public ColorBotContext(DbContextOptions options) : base(options) { }
+        
+        public DbSet<LogMessage> LogMessages { get; set; }
+    }
+}
