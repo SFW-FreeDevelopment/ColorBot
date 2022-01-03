2b688b1e4eed00d4c34422cd608b8dbec5c13cf0
Steven Garberg <steven.garberg.jr@gmail.com>
2022-01-02 23:25:53 -0500

    Implemented Log Command

---
+++ ColorBot.App/Commands/LogCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using ColorBot.App.Models;
+using ColorBot.App.Repositories;
+using Discord.Commands;
+
+namespace ColorBot.App.Commands
+{
+    public class LogCommand : CommandBase
+    {
+        [Command("log")]
+        public async Task HandleCommandAsync(string value)
+        {
+            await Log(value, "log");
+        }
+    }
+}
--- ColorBot.App/Models/LogMessage.cs
+++ ColorBot.App/Models/LogMessage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using Discord.Commands;
 
 namespace ColorBot.App.Models
 {
@@ -11,7 +12,25 @@ namespace ColorBot.App.Models
         public string Command { get; set; }
         public string SubCommand { get; set; }
         public string Author { get; set; }
+        public string ServerId { get; set; }
+        public string ServerName { get; set; }
+        public string ChannelId { get; set; }
+        public string ChannelName { get; set; }
         [NotMapped] public List<string> Mentions { get; set; }
         public DateTime CreatedAt { get; set; }
+        
+        public LogMessage() { }
+
+        public LogMessage(SocketCommandContext context, string message, string command, string subCommand = null)
+        {
+            Author = context.User.Username;
+            ServerId = context.Guild.Id.ToString();
+            ServerName = context.Guild.Name;
+            ChannelId = context.Channel.Id.ToString();
+            ChannelName = context.Channel.Name;
+            Message = message;
+            Command = command;
+            SubCommand = subCommand;
+        }
     }
 }
